a8dc1e6a7fed2822cfeca636e4b68bbf63dccc46
Trevor Allred <trevorallred@gmail.com>
2021-09-28 18:04:41 -0700

    first MVP

--- src/App.js
+++ src/App.js
@@ -1,37 +1,114 @@
-import "./App.css";
-import { useState } from "react";
+import React, { useState } from "react";
 
 function App() {
-  const [changingPlay, changePlay] = useState(false);
-  const [play, setPlay] = useState(12);
-  const [primary, setPrimary] = useState("B");
+  const [play, setPlay] = useState(0);
+  const [player, setPlayer] = useState("");
 
   function DisplayPlay() {
     return (
       <div
-        className="DisplayPlay"
         style={{
           textAlign: "center",
           height: "100vh",
+          color: "white",
         }}
       >
         <div
-          className="Play"
+          style={{
+            flex: 1,
+            fontSize: "40vh",
+          }}
           onClick={() => {
-            changePlay(true);
+            setPlay(0);
           }}
         >
           {play}
         </div>
-        <div className="Primary" onClick={() => {}}>
-          {primary}
+        <div
+          style={{
+            flex: 1,
+            fontSize: "40vh",
+          }}
+          onClick={() => {
+            setPlayer("");
+          }}
+        >
+          {player}
         </div>
       </div>
     );
   }
 
-  if (changingPlay) return <div className="App"></div>;
-  return <DisplayPlay />
+  const plays = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+  const players = ["L", "C", "T", "R", "B"];
+
+  if (play < 1)
+    return (
+      <div
+        style={{
+          padding: "3em",
+        }}
+      >
+        <h1>Pick a Play</h1>
+        <p>Currently selected player: {player}</p>
+        <div
+          style={{
+            flexFlow: "column wrap",
+          }}
+        >
+          {plays.map((play) => {
+            return (
+              <div
+                style={{
+                  fontSize: "4em",
+                  width: "50%",
+                  border: 1,
+                }}
+                onClick={() => {
+                  setPlay(play);
+                }}
+              >
+                {play}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  if (player === "")
+    return (
+      <div
+        style={{
+          padding: "3em",
+        }}
+      >
+        <h1>Pick a Primary Target</h1>
+        <p>Currently selected play: {play}</p>
+        <div
+          style={{
+            flexFlow: "column wrap",
+          }}
+        >
+          {players.map((player) => {
+            return (
+              <div
+                style={{
+                  fontSize: "4em",
+                  width: "50%",
+                  border: 1,
+                }}
+                onClick={() => {
+                  setPlayer(player);
+                }}
+              >
+                {player}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  return <DisplayPlay />;
 }
 
 export default App;
