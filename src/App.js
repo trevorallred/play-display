664fc3660c2313cf14f8c4906df6bac6a46bfb6e
Trevor Allred <trevorallred@gmail.com>
2021-09-28 17:41:54 -0700

    play card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  const [changingPlay, changePlay] = useState(false);
+  const [play, setPlay] = useState(12);
+  const [primary, setPrimary] = useState("B");
+
+  function DisplayPlay() {
+    return (
+      <div
+        className="DisplayPlay"
+        style={{
+          textAlign: "center",
+          height: "100vh",
+        }}
+      >
+        <div
+          className="Play"
+          onClick={() => {
+            changePlay(true);
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {play}
+        </div>
+        <div className="Primary" onClick={() => {}}>
+          {primary}
+        </div>
+      </div>
+    );
+  }
+
+  if (changingPlay) return <div className="App"></div>;
+  return <DisplayPlay />
 }
 
 export default App;
